2f54c2b5a48405b53a8e3a7ce3cf39a287ab37fe
arun <arunmahara124@gmail.com>
2024-01-17 14:53:54 +0545

    update: new certificate template

--- cerificate.py
+++ cerificate.py
@@ -5,35 +5,35 @@ def get_font_size_and_position(text: str, is_hindi: bool) -> tuple:
     text_length = len(text)
     if is_hindi:
         if text_length <= 20:
-            return 85, (230, 170)
+            return 80, (230, 110)
         elif text_length <= 25:
-            return 75, (230, 175)
+            return 75, (230, 115)
         elif text_length <= 30:
-            return 65, (230, 185)
+            return 65, (230, 125)
         elif text_length <= 35:
-            return 55, (235, 200)
+            return 55, (235, 135)
         else:
-            return 45, (235, 210)
+            return 45, (235, 140)
     else:
         if text_length <= 20:
-            return 145, (220, 220)
+            return 145, (220, 230)
         elif text_length <= 25:
-            return 120, (215, 240)
+            return 120, (215, 250)
         elif text_length <= 30:
-            return 100, (210, 260)
+            return 100, (210, 265)
         elif text_length <= 35:
-            return 90, (210, 270)
+            return 90, (210, 275)
         elif text_length <= 40:
-            return 80, (205, 275)
+            return 80, (205, 280)
         else:
-            return 70, (200, 280)
+            return 70, (200, 290)
 
 
 def create_certificate(name: str, in_hindi: str, output_path: str) -> None:
     try:
         print(f"Creating certificate for {name} and saving to {output_path}")
 
-        cerificate_path = "artifacts/certificate_hindi.jpg" if in_hindi else "artifacts/certificate_english.jpg"
+        cerificate_path = "artifacts/cert_hindi.jpg" if in_hindi else "artifacts/cert_eng.jpg"
         font_path = "artifacts/font_hindi.ttf" if in_hindi else "artifacts/font_english.ttf"
 
         img = Image.open(cerificate_path)
