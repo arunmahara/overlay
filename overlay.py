ba386b867f0937fa9837d07541c4d6b0bea9b883
arun <arunmahara124@gmail.com>
2024-02-06 19:50:42 +0545

    fix: masked image

--- overlay.py
+++ overlay.py
@@ -1,48 +1,32 @@
-import cv2
-import numpy as np
+from PIL import Image
 
 
 def overlay_images(bg_path: str, ov_path: str, output_path: str) -> None:
     try:
         print(f"Overlaying {ov_path} on {bg_path} and saving to {output_path}")
 
-        background = cv2.imread(bg_path)
-        overlay = cv2.imread(ov_path)
+        # Open the profile image
+        profile = Image.open(bg_path)
 
-        # Resize images
-        W = 750
-        imgScale = W / background.shape[1]
-        new_background = cv2.resize(
-            background, (int(background.shape[1] * imgScale),
-                         int(background.shape[0] * imgScale)))
+        # Open the logo image
+        logo = Image.open(ov_path)
 
-        W = 700  # Increase this value to make the overlay image bigger
-        imgScale = W / overlay.shape[1]
-        new_overlay = cv2.resize(overlay, (int(overlay.shape[1] * imgScale), int(overlay.shape[0] * imgScale)))
+        # Resize the logo to fit the profile image (adjust the resizing factor to make it bigger)
+        logo_width, logo_height = logo.size
+        profile_width, profile_height = profile.size
+        logo_resized = logo.resize((int(profile_width), int(profile_height)))
 
-        # Create white square
-        square = np.zeros_like(new_background)
-        square.fill(255)
+        # Calculate paste coordinates from the left bottom corner
+        paste_x = 0  # Left
+        paste_y = profile_height - logo_resized.size[1]  # Bottom
 
-        # Adjust the offset for making the overlay image bigger from the left side
-        offset = 0  # You can adjust this value to make it bigger or smaller
-        y, x, _ = new_background.shape
-        square[int(y - new_overlay.shape[0]) - offset:int(y) - offset,
-               :new_overlay.shape[1]] = new_overlay
+        # Overlay the logo on the profile image
+        profile.paste(logo_resized, (paste_x, paste_y), logo_resized)
 
-        # Overlay with specified opacity
-        OPACITY = 0.7
-        added_image = cv2.addWeighted(new_background, 0.6, square, 0.4, 0)
+        # Save the modified profile image
+        profile.save(output_path)
 
-        # Adjust contrast
-        alpha = 1.3
-        beta = -75
-        adjusted = cv2.convertScaleAbs(added_image, alpha=alpha, beta=beta)
-
-        # Save the result
-        cv2.imwrite(output_path, adjusted)
-
-        print(f"Overlayed successfully to {output_path}")
+        print(f"Overlayed successful to {output_path}")
 
     except Exception as e:
         raise e
